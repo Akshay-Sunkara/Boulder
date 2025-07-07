7b30f54658589729e8a8203639ece65ac7f86edd
Akshay-Sunkara <akshaysunkara68@berkeley.edu>
2025-07-07 15:11:18 -0500

    Integrating Flask backend, adding animations

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,9 @@ import { GestureHandlerRootView, PanGestureHandler, State } from 'react-native-g
 import { Switch } from 'react-native-switch';
 import { SelectList } from 'react-native-dropdown-select-list';
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
+import ConfettiCannon from 'react-native-confetti-cannon';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import * as VideoThumbnails from 'expo-video-thumbnails';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDfJxqVElWXx3UNULE1R-2OG1zX4K5lKGo",
@@ -17,9 +19,7 @@ const firebaseConfig = {
   appId: "1:550532324788:web:694144de1c8dc6d4dc66e1",
   measurementId: "G-BMSX4CZ68K"
 };
-
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const Home = () => {
   const [rectSize, setRectSize] = useState({ width: 250, height: 120 });
@@ -50,9 +50,12 @@ const Home = () => {
   const [isUploaded, setUploaded] = useState(false);
 
   const fadeAnim = useRef(new Animated.Value(0)).current;
+  const [resultText, setResultText] = useState('Uploaded Video! Finding Beta...');
+  const [betaFound, setBetaFound] = useState(false);
 
   useEffect(() => {
     if (isUploaded) {
+      setResultText('Uploaded Video! Finding Beta...');
       Animated.timing(fadeAnim, {
         toValue: 1,
         duration: 500,
@@ -64,6 +67,23 @@ const Home = () => {
     }
   }, [isUploaded]);
 
+  useEffect(() => {
+    if (betaFound) {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        setResultText('Beta Found! 🎉');
+        Animated.timing(fadeAnim, {
+          toValue: 1,
+          duration: 400,
+          useNativeDriver: true,
+        }).start();
+      });
+    }
+  }, [betaFound]);
+
   const recordIcon = async () => {
     LayoutAnimation.configureNext({
       duration: 300,
@@ -72,19 +92,47 @@ const Home = () => {
     if (!isRecording) {
       try {
         setRecording(true);
+
         const { uri } = await camRef.current.recordAsync({ mute: true, maxDuration: 50 });
         console.log('Got video at', uri);
-        // const response = await fetch(uri);
-        // const blob = await response.blob();
 
-        // const storage = getStorage(app);
-        // const storageRef = ref(storage, videos/${Date.now()}.mp4);
+        const { uri: thumbnailUri } = await VideoThumbnails.getThumbnailAsync(
+        uri,
+        { time: 1500 }
+        );
+        console.log('Got picture at', thumbnailUri);
 
-        // await uploadBytes(storageRef, blob);
-        // const downloadURL = await getDownloadURL(storageRef);
+        const response = await fetch(uri);
+        const blob = await response.blob()
+        const storage = getStorage(app);
+        const storageRef1 = ref(storage, `Videos/${Date.now()}.mp4`);
+        await uploadBytes(storageRef1, blob);
+        const downloadURL = await getDownloadURL(storageRef1);
+        console.log('Uploaded video:', downloadURL);
 
-        // console.log('Uploaded! File available at:', downloadURL);
+        const thumbResponse = await fetch(thumbnailUri);
+        const thumbBlob = await thumbResponse.blob();
+
+        const thumbRef = ref(storage, `Thumbnails/${Date.now()}.jpg`);
+        await uploadBytes(thumbRef, thumbBlob);
+        const thumbURL = await getDownloadURL(thumbRef);
+        console.log('Uploaded thumbnail:', thumbURL);
+
+        const output = await fetch('http://10.14.175.22:5000/save-thumb', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ thumb_url: thumbURL }),
+        });
+
+        const data = await output.json();
+        console.log('Response from Flask:', data);
+
         setUploaded(true);
+
+        setTimeout(() => {
+          setBetaFound(true);
+        }, 4000);
+
       } catch (e) {
         console.warn('Failed to start recording', e);
         setRecording(false);
@@ -113,12 +161,12 @@ const Home = () => {
 
   const [selected, setSelected] = useState('');
   const data = [
-    { key: '1', value: 'V1 Left #1' },
-    { key: '2', value: 'V2 Left #4' },
-    { key: '3', value: 'V3 Right #2' },
-    { key: '4', value: 'V4 Left #6' },
-    { key: '5', value: 'V3 Right #2' },
-    { key: '6', value: 'V4 Right #7' },
+    { key: '1', value: '🌱  V1  🌱' },
+    { key: '2', value: '🍼  V2  🍼' },
+    { key: '3', value: '🐣  V3  🐣' },
+    { key: '4', value: '🌿  V4  🌿' },
+    { key: '5', value: '🪴  V3  🪴' },
+    { key: '6', value: '🏆  V6  🏆' },
   ];
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(s => !s);
@@ -153,11 +201,11 @@ const Home = () => {
               style={styles.list}
               boxStyles={{ width: 110, padding: 0, borderColor: 'white', top: 22, left: 10 }}
               inputStyles={{ color: 'white' }}
-              dropdownStyles={{ width: 110, borderColor: 'white', top: 22, left: 10, height: 90 }}
-              dropdownTextStyles={{ color: 'white', fontWeight: 600 }}
+              dropdownStyles={{ width: 110, borderColor: 'white', top: 18, left: 10, height: 90, alignItems: 'center'}}
+              dropdownTextStyles={{ color: 'white', fontWeight: 600}}
               placeholder={'V1'}
               search={false}
-              arrowicon={<Icon name="chevron-down-outline" size={20} color="white" />}
+              arrowicon={<Icon name="chevron-down-outline" size={20} color="white" left={5}/>}
             />
           </View>
           <View style={[styles.rectangleOverlay, { width: rectSize.width, height: rectSize.height, maxHeight: 500, maxWidth: 300 }]}>
@@ -216,8 +264,18 @@ const Home = () => {
       </View>
       <View style={styles.resultsContainer}>
         <Animated.View style={[styles.resultsDisplay, { opacity: fadeAnim }]}>
-          <Text style={styles.resultText}>Uploaded Video! Finding Beta...</Text>
+          <Text style={styles.resultText}>{resultText}</Text>
         </Animated.View>
+        {betaFound && (
+          <ConfettiCannon
+            count={50}
+            origin={{ x: 200, y: -10 }}
+            fadeOut={true}
+            explosionSpeed={350}
+            fallSpeed={2500}
+            colors={['rgba(181,21,57,1)', '#fff']}
+          />
+        )}
       </View>
     </GestureHandlerRootView>
   );
@@ -256,3 +314,4 @@ const styles = StyleSheet.create({
   resultsDisplay: { backgroundColor: 'rgba(0,0,0,0.5)', width: 180, height: 25, borderRadius: 33, justifyContent: 'center', alignItems: 'center' },
   resultText: { color: 'white', fontSize: 12 },
 });
+
